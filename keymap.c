6b8dd06dbc2b2822481da421b998dbfb0695cc09
Peter Hatina <peter.hatina@kistler.com>
2018-07-10 10:15:37 +0200

    Update layout wrt brackets, parenthesis, ...
    Signed-off-by: Peter Hatina <peter.hatina@kistler.com>

--- keymap.c
+++ keymap.c
@@ -18,22 +18,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [0] = LAYOUT_ergodox(
         /* left hand */
-        KC_ESCAPE, KC_1,     KC_2,           KC_3,    KC_4,    KC_5, KC_LEFT,      // 7
-        KC_TAB,    KC_Q,     KC_W,           KC_E,    KC_R,    KC_T, KC_MINUS,     // 7
-        KC_LCTL,   KC_A,     KC_S,           KC_D,    KC_F,    KC_G,               // 6
-        KC_LSHIFT, KC_Z,     KC_X,           KC_C,    KC_V,    KC_B, KC_LBRACKET,  // 7
-        KC_LCTL,   KC_QUOTE, KC_TRANSPARENT, KC_LGUI, KC_LALT,                     // 5
+        KC_ESCAPE, KC_1,        KC_2,           KC_3,    KC_4,    KC_5, KC_LEFT,      // 7
+        KC_TAB,    KC_Q,        KC_W,           KC_E,    KC_R,    KC_T, KC_MINUS,     // 7
+        KC_LCTL,   KC_A,        KC_S,           KC_D,    KC_F,    KC_G,               // 6
+        KC_LSHIFT, KC_Z,        KC_X,           KC_C,    KC_V,    KC_B, KC_LBRACKET,  // 7
+        KC_LCTL,   KC_CAPSLOCK, KC_TRANSPARENT, KC_LGUI, KC_LALT,                     // 5
 
-        KC_TRANSPARENT, KC_INSERT,          // 2
-        KC_HOME,                            // 1
-        KC_SPACE,       KC_BSPACE, KC_END,  // 3
+        KC_TRANSPARENT,  KC_INSERT,          // 2
+        KC_HOME,                             // 1
+        LT(2, KC_SPACE), KC_BSPACE, KC_END,  // 3
 
         /* right hand */
-        KC_RIGHT,    KC_6,     KC_7, KC_8,  KC_9,        KC_0,      KC_MINUS,
-        KC_EQUAL,    KC_Y,     KC_U,        KC_I,        KC_O,      KC_P,     KC_BSLASH,
-        KC_H,        KC_J,     KC_K,        KC_L,        KC_SCOLON, KC_QUOTE,
-        KC_RBRACKET, KC_N,     KC_M,        KC_COMMA,    KC_DOT,    KC_SLASH, KC_LSHIFT,
-        KC_GRAVE,    KC_QUOTE, KC_LBRACKET, KC_RBRACKET, MO(1),
+        KC_RIGHT,    KC_6,        KC_7,        KC_8,        KC_9,      KC_0,     KC_MINUS,
+        KC_EQUAL,    KC_Y,        KC_U,        KC_I,        KC_O,      KC_P,     KC_BSLASH,
+        KC_H,        KC_J,        KC_K,        KC_L,        KC_SCOLON, KC_QUOTE,
+        KC_RBRACKET, KC_N,        KC_M,        KC_COMMA,    KC_DOT,    KC_SLASH, KC_LSHIFT,
+        KC_GRAVE,    S(KC_MINUS), KC_LBRACKET, KC_RBRACKET, MO(1),
 
         KC_PSCREEN, KC_TRANSPARENT,
         KC_PGUP,
@@ -42,26 +42,48 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [1] = LAYOUT_ergodox(
         /* left hand */
         KC_GRAVE,       KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,            KC_TRANSPARENT,
-        KC_TRANSPARENT, VIM_QUIT,       VIM_WRITE,      KC_MS_UP,       KC_TRANSPARENT, KC_TRANSPARENT,   KC_TRANSPARENT,
-        KC_CAPSLOCK,    KC_TRANSPARENT, KC_MS_LEFT,     KC_MS_DOWN,     KC_MS_RIGHT,    KC_TRANSPARENT,
+        KC_TRANSPARENT, VIM_QUIT,       KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,   KC_TRANSPARENT,
+        KC_TRANSPARENT, KC_TRANSPARENT, VIM_WRITE,      KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
         KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, VIM_FUZZY_BUFFER, KC_LCBR,
         KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, LALT(KC_SPACE), KC_TRANSPARENT,
 
         KC_TRANSPARENT, KC_TRANSPARENT,
         KC_TRANSPARENT,
-        KC_MS_BTN1,     KC_MS_BTN2,     KC_TRANSPARENT,
+        KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
 
         /* right hand */
         KC_PSCREEN,        KC_F6,           KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_F11,
-        KC_TRANSPARENT,    KC_TRANSPARENT,  KC_TRANSPARENT, KC_TRANSPARENT, VIM_FUZZY_OPEN, KC_TRANSPARENT, KC_F12,
-        KC_LEFT,           KC_DOWN,         KC_UP,KC_RIGHT, KC_TRANSPARENT, KC_TRANSPARENT,
-        KC_RCBR,           KC_TRANSPARENT,  KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+        KC_TRANSPARENT,    KC_TRANSPARENT,  KC_LPRN,        KC_RPRN,        VIM_FUZZY_OPEN, KC_TRANSPARENT, KC_F12,
+        KC_TRANSPARENT,    KC_LCBR,         KC_RCBR,        KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+        KC_RCBR,           KC_TRANSPARENT,  KC_LBRC,        KC_RBRC,        KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
         KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP, KC_AUDIO_MUTE,  KC_TRANSPARENT, KC_TRANSPARENT,
 
         KC_TRANSPARENT, KC_TRANSPARENT,
         KC_TRANSPARENT,
         KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT),
 
+  [2] = LAYOUT_ergodox(
+        /* left hand */
+        KC_GRAVE,       KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,            KC_TRANSPARENT,
+        KC_TRANSPARENT, VIM_QUIT,       VIM_WRITE,      KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,   KC_TRANSPARENT,
+        KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+        KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, VIM_FUZZY_BUFFER, KC_LCBR,
+        KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, LALT(KC_SPACE), KC_TRANSPARENT,
+
+        KC_TRANSPARENT, KC_TRANSPARENT,
+        KC_TRANSPARENT,
+        KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+
+        /* right hand */
+        KC_PSCREEN,        KC_F6,           KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_F11,
+        KC_TRANSPARENT,    KC_TRANSPARENT,  KC_TRANSPARENT, KC_TRANSPARENT, VIM_FUZZY_OPEN, KC_TRANSPARENT, KC_F12,
+        KC_LEFT,           KC_DOWN,         KC_UP,          KC_RIGHT,       KC_TRANSPARENT, KC_TRANSPARENT,
+        KC_RCBR,           KC_TRANSPARENT,  KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+        KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP, KC_AUDIO_MUTE,  KC_TRANSPARENT, KC_TRANSPARENT,
+
+        KC_TRANSPARENT, KC_TRANSPARENT,
+        KC_TRANSPARENT,
+        KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT),
 };
 
 const uint16_t PROGMEM fn_actions[] = {
